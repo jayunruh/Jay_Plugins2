273aaf4b59f712ecde20209974f55a81ef35c40a
jayunruh <jru@stowers.org>
2018-04-17 08:30:43 -0500

    update

--- set_imaris_spot_traj_jru_v1.java
+++ set_imaris_spot_traj_jru_v1.java
@@ -11,6 +11,7 @@ import ij.gui.*;
 import java.awt.*;
 import ij.plugin.*;
 import jguis.*;
+import jalgs.*;
 
 public class set_imaris_spot_traj_jru_v1 implements PlugIn {
 
@@ -24,21 +25,38 @@ public class set_imaris_spot_traj_jru_v1 implements PlugIn {
 		boolean pixunits=gd.getNextBoolean();
 		float rad=(float)gd.getNextNumber();
 		int sel=(Integer)jutils.runPW4VoidMethod(iw[0],"getSelected");
-		int[] npts=(int[])jutils.runPW4VoidMethod(iw[0],"getNpts");
-		float[] x=((float[][])jutils.runPW4VoidMethod(iw[0],"getXValues"))[sel];
-		float[] y=((float[][])jutils.runPW4VoidMethod(iw[0],"getYValues"))[sel];
-		if(x.length>npts[sel]){
-			x=algutils.get_subarray(x,0,npts[sel]);
-			y=algutils.get_subarray(y,0,npts[sel]);
-		}
-		float[] z=new float[npts[sel]];
-		if(jutils.isPlot(iw[0])){
-			z=((float[][][])jutils.runPW4VoidMethod(iw[0],"getZValues"))[0][sel];
-			if(x.length>npts[sel]) z=algutils.get_subarray(z,0,npts[sel]);
+		int[] npts=((int[][])jutils.runPW4VoidMethod(iw[0],"getNpts"))[0];
+		float[] x,y,z;
+		if(npts[sel]==1){
+			//here we have a bunch of single points in our trajectory window
+			float[][] xt=((float[][])jutils.runPW4VoidMethod(iw[0],"getXValues"));
+			float[][] yt=((float[][])jutils.runPW4VoidMethod(iw[0],"getYValues"));
+			float[][] zt=((float[][][])jutils.runPW4VoidMethod(iw[0],"getZValues"))[0];
+			x=new float[npts.length];
+			y=new float[npts.length];
+			z=new float[npts.length];
+			for(int i=0;i<npts.length;i++){
+				x[i]=xt[i][0];
+				y[i]=yt[i][0];
+				z[i]=zt[i][0];
+			}
+		} else {
+			x=((float[][])jutils.runPW4VoidMethod(iw[0],"getXValues"))[sel];
+			y=((float[][])jutils.runPW4VoidMethod(iw[0],"getYValues"))[sel];
+			if(x.length>npts[sel]){
+				x=(float[])algutils.get_subarray(x,0,npts[sel]);
+				y=(float[])algutils.get_subarray(y,0,npts[sel]);
+			}
+			z=new float[npts[sel]];
+			if(jutils.is3DPlot(iw[0])){ //this should be 3D plot, right?
+				z=((float[][][])jutils.runPW4VoidMethod(iw[0],"getZValues"))[0][sel];
+				if(x.length>npts[sel]) z=(float[])algutils.get_subarray(z,0,npts[sel]);
+			}
 		}
 		String[] annot=(String[])jutils.runPW4VoidMethod(iw[0],"getAnnotations");
 		int[] timeindices=new int[npts[sel]];
-		if(annot!=null){
+		if(npts[sel]==1) timeindices=new int[npts.length];
+		if(annot!=null && npts[sel]>1){
 			int start=(int)Float.parseFloat(annot[sel]);
 			for(int i=0;i<npts[sel];i++){
 				timeindices[i]=start+i;
